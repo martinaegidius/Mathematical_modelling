5ee077457f8ed407e5303b07d5c86323bfbbfca5
Martin Aegidius <gygridd@gmail.com>
2022-02-18 10:13:00 +0100

    added disco-plots

---
+++ toyProblem_V2.py
@@ -0,0 +1,223 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import matplotlib
+import numpy as np
+from scipy import ndimage as ndi
+
+
+class OpticalFlow():
+    def __init__(self):
+        self.runAll()
+        
+    def runAll(self):
+       self.prepImages()
+       self.gradientCalc()
+    #    self.showGradies(self.xGrad)
+       self.kernelConv()
+    #    self.showGradies(self.sobelKernelX)
+       self.gaussConvelution()
+       #self.showGradies(self.gaussConvTest)
+       #self.CornerPixelGradient()
+       self.LUKASBOI()
+       self.quiver_plot()
+        
+    def prepImages(self):
+        path = os.path.dirname(__file__)
+        path = os.path.join(path,'../../toyProblem_F22/')
+        #path = '/Users/frederikgade/Documents/DTU/6. Semester/02526 Mathematical Modeling/Excersize 1/toyProblem_F22/'
+        imageList = os.listdir(path)
+        images = [img.imread(path+os.sep+imageName) for imageName in sorted(imageList)]
+        arrayList = [np.asarray(image) for image in images]
+        self.grayImages = np.array([self.rgb2gray(colorImage) for colorImage in arrayList])
+        
+    def rgb2gray(self,rgb):
+        r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        return gray
+
+    def gradientCalc(self):
+        self.xGrad = np.empty((len(self.grayImages[:,0,0]),256,255))
+        self.yGrad = np.empty((len(self.grayImages[:,0,0]),255,256))
+        for i, dim in enumerate(self.grayImages[:,0,0]):
+           for j, dim in enumerate(self.grayImages[0,:,0]):
+               self.xGrad[i,j,:] = self.grayImages[i,j,1:]-self.grayImages[i,j,0:-1]
+               self.yGrad[i,:,j] = self.grayImages[0,1:,j]-self.grayImages[0,0:-1,j]
+   
+    def showGradies(self,arr):
+        plt.gray()
+        for i,boi in enumerate(arr[:,0,0]):
+            plt.imshow(arr[i,:,:])
+            plt.pause(0.02)
+            plt.show
+
+    def kernelConv(self):
+        self.sobelKernelX = np.empty((len(self.grayImages[:,0,0]),256,256))
+        self.sobelKernelY = np.empty((len(self.grayImages[:,0,0]),256,256))
+        for i in range(len(self.grayImages)):
+            self.sobelKernelX[i,:,:] = ndi.sobel(self.grayImages[i,:,:],axis=1)
+            self.sobelKernelY[i,:,:] = ndi.sobel(self.grayImages[i,:,:],axis=0)
+
+    def gaussConvelution(self,sigmaChoice=4):
+        self.gaussConv = np.empty((len(self.grayImages[:,0,0]),256,256))
+        self.tVec = np.linspace(0,63,num=64)
+        for i in range(len(self.grayImages)):
+            for j in range(len(self.grayImages[0][:,0])):
+                self.gaussConv[i,:,j] = ndi.gaussian_filter1d(self.grayImages[i,:,j],sigma=sigmaChoice)
+        
+        for i in range(len(self.grayImages)):
+            for j in range(len(self.grayImages[0][0,:])):
+                self.gaussConv[i,j,:] = ndi.gaussian_filter1d(self.grayImages[i,j,:],sigma=sigmaChoice)
+
+        self.tVec = ndi.gaussian_filter1d(self.tVec,sigma=sigmaChoice)
+
+
+        self.gaussConvTest = np.empty((len(self.grayImages[:,0,0]),256,256))
+        self.gaussConvTest = ndi.gaussian_filter(self.grayImages,sigma=sigmaChoice,order=(0,0,1))
+
+
+    def CornerPixelGradient(self):
+        sigmaChoice=4
+        pixel = [12,12]
+        #finding cornerpoint for loop -> [11,11]
+        pixelGradX = np.empty((3,3))
+        pixelGradY = np.empty((3,3))
+        pixel = [11,11]
+        
+        self.analPix = self.grayImages[0,11:14,11:14]
+        for j in range(0,3):
+            pixelGradX[:,j] = ndi.gaussian_filter1d(self.analPix[:,j],sigma=sigmaChoice)
+            pixelGradY[j,:] = ndi.gaussian_filter1d(self.analPix[j,:],sigma=sigmaChoice)
+        
+    def LUKASBOI(self,N=3,stride=40):
+        ''' Does Lukas Kanabe on all images in variable self.grayImages.
+
+        Args:
+            N (int): N is one side in the square around the pixels to be processed. (Defualt is 3)
+            stride (int): The stride for pixels to process. (Default is 40)
+
+        Returns:
+            Nothing. Does however add variables to class instance. The primary are:
+            self.coordinates, an M long list containing sublists with coordinates to
+            pixels being processed. 
+            self.gradiants, an k x M long list of lists containing matching gradiants
+            to coordinates self.coordinates for every frame.      
+        '''
+        if N < 1:
+            N=3
+        if (stride < N or stride < 1):
+            stride = 40
+            if stride<N:
+                stride=N+5
+        
+        minX = N
+        minY = N
+        maxX = len(self.grayImages[0,:,0])-N
+        maxY = len(self.grayImages[0,0,:])-N
+
+        N = int(N/2)
+        
+        global coordinates
+        global gradiants
+        
+        
+        coordinates = []
+        for i in range(minX,maxX,stride):
+            for j in range (minY,maxY,stride):
+                coordinates.append([i,j])
+
+        self.coordinates = coordinates
+        self.gradiants = np.empty((len(self.grayImages),len(self.coordinates),2,1))
+
+        for imageNumber, image in enumerate(self.grayImages):    
+            for i in range(minX,maxX,stride):
+                for j in range(minY,maxY,stride):
+                    gradX = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(0,1,0),mode='nearest')
+                    gradY = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(0,0,1),mode='nearest')
+                    vT = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(1,0,0),mode='nearest')
+
+
+
+                    A = np.concatenate((np.reshape(gradX[imageNumber], (-1,1)),np.reshape(gradY[imageNumber],(-1,1))),axis=1)
+                    b = np.reshape(vT[imageNumber],(-1,1))
+                    
+                    if j == minY:
+                        self.testOutput = np.linalg.lstsq(A,b,rcond=None)[0:1]
+                    else:
+                        self.testOutput = np.concatenate((self.testOutput,np.linalg.lstsq(A,b,rcond=None)[0:1]))
+
+                if (i==minX):
+                    self.mellemstop = self.testOutput
+                else:
+                    self.mellemstop = np.concatenate((self.mellemstop,self.testOutput))
+
+            self.gradiants[imageNumber]=self.mellemstop
+
+
+    def quiver_plot(self):
+        '''function which takes as input an array which defines the 
+        coordinates for pixels for which flow-lines are requested 
+        in format (x,y). 
+        the pixel-array is to be matched with corresponding values stored in self.testOutput in some sort of sensible way.
+        '''
+        
+        #generating a diagonal pointsample (should come from other function)
+        #p = np.linspace(0,256,num = 30).astype(int) #pairwise array of pixels requested analyzed (should come from LUKASBOI)
+        global q 
+        #q = np.vstack((p,p)).T
+        q = np.array(self.coordinates)
+        
+        global dataHolder ##create a matrix for holding all gradients for points. Should be generated from another function
+        #in format [frames x pointvalues (pairwise)]
+        #dataHolder = np.zeros((len(self.grayImages),len(q)))
+        
+        dataHolder = self.gradiants
+        print(dataHolder.shape)
+        #concatenate all flow measurements
+        #for j in range(len(self.grayImages)-1):
+            #for i in range(0,len(q),2):
+                #we are loosing last frame. Should maybe be corrected to len(grayImages)
+                
+                #following should be a oneliner somehow 
+       #         dataHolder[j][i] = self.testOutput[j][0]
+       #         dataHolder[j][i+1] = self.testOutput[j][1]
+                #thus this is not completely dynamic, sicing would fix
+        
+                
+        
+        ##step for making proper normalized colorbar
+        #part 1 remove outliers from dataset
+        global lengths 
+        lengths = np.zeros((dataHolder.shape[0],dataHolder.shape[1])) #for storing vector norms for each pixel for each frame
+        for j in range(dataHolder.shape[0]): #loop over frames 
+            for i in range(0,dataHolder.shape[1]): #loop over pixel of interest
+                lengths[j][i] = np.linalg.norm(dataHolder[j,i,:,:]) ##fill every col of lengths
+        
+        #gross outlier-removal (floor outliers to mean value)
+        tolerance = 3*np.mean(lengths)
+        lengths[lengths>tolerance] = np.mean(lengths) #remember that norms are always positive, thus no abs() needed
+        upperLimit = (np.amax(lengths)) #find maximal value of all pixels through all frames to define top-edge of colorbar
+        
+        #create colorbar element which forces normalization to range of lengths
+        norm = matplotlib.colors.Normalize(vmin=0,vmax=upperLimit)
+        norm.autoscale(lengths)
+        
+       #plot all images with colorbar. 
+        numImages = 50#dataHolder.shape[0] #last frame has been lost earlier :-(
+        
+        for i in range(numImages): #imagewise 
+            fig = plt.imshow(self.grayImages[i,:,:],cmap='gray') #plot frame of interest
+            for j in range(0,dataHolder.shape[1]): #loop over all columns in dataHolder-array for the corresponding frame
+                #print(j) for debugging
+                plt.quiver(q[j,0], q[j,1], dataHolder[i,j,0,:], dataHolder[i,j,1,:], lengths[i,j],cmap='hsv',norm=norm)#arguments: startpoint(x,y),vector(x,y),"measurement" for colormap, create arrow-colors
+                
+            plt.colorbar()
+            #plt.clim(0,upperLimit) #max should be max of array after outliers rmvd
+            plt.xlim([0,250]) #forces constant axes
+            plt.ylim([250,0]) #forces constant axes (reversed) 
+            plt.show()
+        #test if last frame looks proper
+        print("last bottom right arrow should have color corresponding to " +str(lengths[numImages-1,-1]))
+        print("last bottom right arrow should have direction x: "+str(dataHolder[numImages-1,-1,0,:])+" and y: "+ str(dataHolder[numImages-1,-1,1,:]))#whoops - only for debugging. 
+
+OpticalFlow()
