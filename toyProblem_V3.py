ed62439a1e1de5aa494e3494ce4d02cd9542cc76
Martin Aegidius <gygridd@gmail.com>
2022-02-20 12:31:07 +0100

    done discowuiver with fine settings

--- toyProblem_V3.py
+++ toyProblem_V3.py
@@ -20,12 +20,14 @@ class OpticalFlow():
        #self.showGradies(self.gaussConvTest)
        #self.CornerPixelGradient()
        self.LUKASBOI(N=2,stride=10)
-       #self.quiver_plot()
-       self.simple_plot()
+       self.quiver_plot()
+       #self.simple_plot()
         
     def prepImages(self):
         path = os.path.dirname(__file__)
         path = os.path.join(path,'../../toyProblem_F22/')
+        #path = os.path.join(path,'video/frames/')
+        
         #path = '/Users/frederikgade/Documents/DTU/6. Semester/02526 Mathematical Modeling/Excersize 1/toyProblem_F22/'
         imageList = os.listdir(path)
         images = [img.imread(path+os.sep+imageName) for imageName in sorted(imageList)]
@@ -92,11 +94,9 @@ class OpticalFlow():
         
     def LUKASBOI(self,N=7,stride=40):
         ''' Does Lukas Kanabe on all images in variable self.grayImages.
-
         Args:
             N (int): N is one side in the square around the pixels to be processed. (Defualt is 3)
             stride (int): The stride for pixels to process. (Default is 40)
-
         Returns:
             Nothing. Does however add variables to class instance. The primary are:
             self.coordinates, an M long list containing sublists with coordinates to
@@ -115,43 +115,35 @@ class OpticalFlow():
         minY = N
         maxX = len(self.grayImages[0,:,0])-N
         maxY = len(self.grayImages[0,0,:])-N
-
         N = int(N/2)
         
         # global coordinates
         # global gradiants
         
-        
+        sigmaChoice = 1.8
         coordinates = []
         for i in range(minX,maxX,stride):
             for j in range (minY,maxY,stride):
                 coordinates.append([i,j])
-
         self.coordinates = coordinates
         self.gradiants = np.empty((len(self.grayImages),len(self.coordinates),2,1))
-
+        gradX = ndi.gaussian_filter(self.grayImages[:,:,:],sigma=sigmaChoice,order=(0,1,0),mode='nearest')
+        gradY = ndi.gaussian_filter(self.grayImages[:,:,:],sigma=sigmaChoice,order=(0,0,1),mode='nearest')
+        vT = ndi.gaussian_filter(self.grayImages[:,:,:],sigma=sigmaChoice,order=(1,0,0),mode='nearest')
         for imageNumber, image in enumerate(self.grayImages):    
             for i in range(minX,maxX,stride):
                 for j in range(minY,maxY,stride):
-                    gradX = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(0,1,0),mode='nearest')
-                    gradY = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(0,0,1),mode='nearest')
-                    vT = ndi.gaussian_filter(self.grayImages[:,i-N:i+N+1,j-N:j+N+1],sigma=N,order=(1,0,0),mode='nearest')
-
-
-
-                    A = np.concatenate((np.reshape(gradX[imageNumber], (-1,1)),np.reshape(gradY[imageNumber],(-1,1))),axis=1)
-                    b = np.reshape(vT[imageNumber],(-1,1))
+                    A = np.concatenate((np.reshape(gradX[imageNumber,i-N:i+N+1,j-N:j+N+1], (-1,1)),np.reshape(gradY[imageNumber,i-N:i+N+1,j-N:j+N+1],(-1,1))),axis=1)
+                    b = np.reshape(vT[imageNumber,i-N:i+N+1,j-N:j+N+1],(-1,1))
                     
                     if j == minY:
                         self.testOutput = np.linalg.lstsq(A,b,rcond=None)[0:1]
                     else:
                         self.testOutput = np.concatenate((self.testOutput,np.linalg.lstsq(A,b,rcond=None)[0:1]))
-
                 if (i==minX):
                     self.mellemstop = self.testOutput
                 else:
                     self.mellemstop = np.concatenate((self.mellemstop,self.testOutput))
-
             self.gradiants[imageNumber]=self.mellemstop
 
 
@@ -208,70 +200,42 @@ class OpticalFlow():
         in format (x,y). 
         the pixel-array is to be matched with corresponding values stored in self.testOutput in some sort of sensible way.
         '''
-        
-        #generating a diagonal pointsample (should come from other function)
-        #p = np.linspace(0,256,num = 30).astype(int) #pairwise array of pixels requested analyzed (should come from LUKASBOI)
-        global q 
-        #q = np.vstack((p,p)).T
-        q = np.array(self.coordinates)
-        
-        global dataHolder ##create a matrix for holding all gradients for points. Should be generated from another function
-        #in format [frames x pointvalues (pairwise)]
-        #dataHolder = np.zeros((len(self.grayImages),len(q)))
-        
-        dataHolder = self.gradiants
-        print(dataHolder.shape)
-        #concatenate all flow measurements
-        #for j in range(len(self.grayImages)-1):
-            #for i in range(0,len(q),2):
-                #we are loosing last frame. Should maybe be corrected to len(grayImages)
-                
-                #following should be a oneliner somehow 
-       #         dataHolder[j][i] = self.testOutput[j][0]
-       #         dataHolder[j][i+1] = self.testOutput[j][1]
-                #thus this is not completely dynamic, sicing would fix
-        
-                
+        global xygrads
+        global p
+        xygrads = self.gradiants.squeeze() #remove unecessary 4th dim
+        p = self.coordinates
         
         ##step for making proper normalized colorbar
-        #part 1 remove outliers from dataset
         global lengths 
-        lengths = np.zeros((dataHolder.shape[0],dataHolder.shape[1])) #for storing vector norms for each pixel for each frame
-        for j in range(dataHolder.shape[0]): #loop over frames 
-            for i in range(0,dataHolder.shape[1]): #loop over pixel of interest
-                lengths[j][i] = np.linalg.norm(dataHolder[j,i,:,:]) ##fill every col of lengths
+        lengths = np.zeros((xygrads.shape[0],xygrads.shape[1])) #for storing vector norms for each pixel for each frame
+        for i in range(len(self.grayImages)-1): #loop over frames 
+            for j in range(0,lengths.shape[1]): #loop over every pixel
+                lengths[i][j] = abs(xygrads[i,j,0])+abs(xygrads[i,j,1]) # calculate vector norm of each x,y-gradient
         
-        #gross outlier-removal (floor outliers to mean value)
-        #tolerance = 100*np.mean(lengths)
-        #lengths[lengths>tolerance] = np.mean(lengths) #remember that norms are always positive, thus no abs() needed
-        upperLimit = (np.percentile(lengths,95)) #define upper edge of colorbar as 95 percentile of lengths
-        #upperLimit = np.amax(lengths)
-        lowerLimit = np.percentile(lengths,70)
-        lengths[lengths<lowerLimit] = np.NAN
+        #only interested in very small percentage of movement for tracking
+        upperLimit = (np.percentile(lengths,97)) #define upper edge of colorbar as 95 percentile of lengths
+        lowerLimit = np.percentile(lengths,90)
         
         #create colorbar element which forces normalization to range of lengths
         norm = matplotlib.colors.Normalize(vmin=lowerLimit,vmax=upperLimit,clip=False)
         norm.autoscale(lengths)
         
        #plot all images with colorbar. 
-        numImages = 50#dataHolder.shape[0] #last frame has been lost earlier :-(
+        numImages = len(self.grayImages)#dataHolder.shape[0] #last frame has been lost earlier :-(
         
-        for i in range(numImages): #imagewise 
+        for i in range(numImages-1): #imagewise 
             fig = plt.imshow(self.grayImages[i,:,:],cmap='gray') #plot frame of interest
-            for j in range(0,dataHolder.shape[1]): #loop over all columns in dataHolder-array for the corresponding frame
+            for j in range(0,xygrads.shape[1]): #loop over all columns in dataHolder-array for the corresponding frame
                 #print(j) for debugging
-                #plt.quiver(q[j,0], q[j,1], dataHolder[i,j,0,:], dataHolder[i,j,1,:], lengths[i,j],cmap='jet',norm=norm)#arguments: startpoint(x,y),vector(x,y),"measurement" for colormap, create arrow-colors
-                plt.plot(q[j,0],q[j,1],'o',color='red',markersize=2)
-                plt.plot(np.array([q[j,0],q[j,1]+2*dataHolder[i,j,1,:]]),np.array([q[j,1],q[j,0]+2*dataHolder[i,j,0,:]]),'-',color = 'red', markersize = 15)
-                
-                
-            #plt.colorbar()
-            #plt.clim(lowerLimit,upperLimit) #max should be max of array after outliers rmvd
-            plt.xlim([0,250]) #forces constant axes
-            plt.ylim([250,0]) #forces constant axes (reversed) 
+                if (lengths[i][j]<lowerLimit or lengths[i][j]>upperLimit):
+                    pass
+                else:
+                    plt.quiver(p[j][1], p[j][0], -xygrads[i,j,1], xygrads[i,j,0], lengths[i,j],cmap='jet',norm=norm)#arguments: startpoint(x,y),vector(x,y),"measurement" for colormap, create arrow-colors
+                        
+            plt.colorbar()
+            plt.clim(lowerLimit,upperLimit) #max should be max of array after outliers rmvd
+            plt.xlim([0,256]) #forces constant axes
+            plt.ylim([256,0]) #forces constant axes (reversed) 
             plt.show()
-        #test if last frame looks proper
-        print("last bottom right arrow should have color corresponding to " +str(lengths[numImages-1,-1]))
-        print("last bottom right arrow should have direction x: "+str(dataHolder[numImages-1,-1,0,:])+" and y: "+ str(dataHolder[numImages-1,-1,1,:]))#whoops - only for debugging. 
-
+        
 OpticalFlow()
--- videoToFrames.py
+++ videoToFrames.py
@@ -5,7 +5,7 @@ import cv2
 import os 
 
 path = os.path.dirname(os.path.abspath(__file__))
-vidpath = os.path.join(path,'video/SampleVideo_1280x720_10mb.mp4')
+vidpath = os.path.join(path,'video/god_video.mp4')
 savepath = os.path.join(path,'video/frames')
 
 if not os.path.exists(savepath):
@@ -19,15 +19,22 @@ def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite(os.path.join(savepath,"image"+str(count)+".jpg"), image)     # save frame as JPG file
+        image = cv2.resize(image,(256,256))
+        cv2.imwrite(os.path.join(savepath,"image"+countstr+".jpg"), image)     # save frame as JPG file
     return hasFrames
 
 sec = 0
-frameRate = 0.5 #//it will capture image in each 0.5 second
-count=1
+frameRate = 0.1 #//it will capture image in each 0.1 second
+count = 1
+if count==1:
+    countstr="01"
 success = getFrame(sec)
 while success:
     count = count + 1
+    if(count<10):
+        countstr = "0"+str(count)
+    else:
+        countstr = str(count)
     sec = sec + frameRate
     sec = round(sec, 2)
     success = getFrame(sec)
